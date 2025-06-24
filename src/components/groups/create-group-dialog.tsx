81791e210bbfb14f7e8126a658b2e58b43726b0c
Yashraj Jangra <jangrayash1505@gmail.com>
2025-06-24 10:36:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/groups/create-group-dialog.tsx
+++ src/components/groups/create-group-dialog.tsx
@@ -17,7 +17,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Icons } from "@/components/icons";
