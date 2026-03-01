2faf8ebcc4e1d32179b8dc94995cbc2ac88e3efe
Yashraj Jangra <jangrayash1505@gmail.com>
2026-03-01 11:58:41 +0000

    recently i asked u to change this hover background red color its not fix

--- src/components/layout/user-nav.tsx
+++ src/components/layout/user-nav.tsx
@@ -95,7 +95,7 @@ export function UserNav() {
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={handleLogout} className="text-destructive focus:bg-accent focus:text-accent-foreground">
+        <DropdownMenuItem onClick={handleLogout} className="focus:bg-destructive/20 focus:text-destructive">
           <Icons.Logout className="mr-2 h-4 w-4" />
           <span>Log out</span>
         </DropdownMenuItem>
